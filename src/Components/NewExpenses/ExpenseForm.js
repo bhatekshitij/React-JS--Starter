2b8fa78b155a26a951d97b0656a87221dc96166c
Kshitij <kshitijbhate@hotmail.com>
2022-11-08 23:09:08 +0530

    Added minor changes to code

--- src/Components/NewExpenses/ExpenseForm.js
+++ src/Components/NewExpenses/ExpenseForm.js
@@ -5,30 +5,29 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
 
-    // const [enteredTitle, setEntertedTitle] = useState('')
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+    const [enteredTitle, setEntertedTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
-    const [userInput, setUserInput] = useState(
-        {
-            enteredTitle: '',
-            enteredAmount: '',
-            entredDate: '',
-        }
+    // const [userInput, setUserInput] = useState(
+    //     {
+    //         enteredTitle: '',
+    //         enteredAmount: '',
+    //         entredDate: '',
+    //     }
 
-    );
+    // );
 
 
     const titleChangeHandler = (event) => {
-        // setEntertedTitle(event.target.value);
-        // console.log(enteredTitle);
+        setEntertedTitle(event.target.value);
+        console.log(enteredTitle);
 
-        setUserInput({
-            ...userInput,//-> ...-> these three dots basically represents to copy all the elements in the array and store it in a variable for temporary reasons. here userInput is the variable where all the elements are copied.
-            enteredTitle: event.target.value
-        });
+        // setUserInput({
+        //     ...userInput,//-> ...-> these three dots basically represents to copy all the elements in the array and store it in a variable for temporary reasons. here userInput is the variable where all the elements are copied.
+        //     enteredTitle: event.target.value
+        // });
 
-        console.log(userInput.json);
     }
 
 
@@ -41,29 +40,43 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value);
 
     }
+
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+
+        }
+        console.log(expenseData);
+
+    }
     return (
-        <form>
-            <div class="new-expense__controls">
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
                 <div class="new-expense__control">
                     <label for="title">title</label>
                     <input type="text" onChange={titleChangeHandler} />
                 </div>
 
 
-                <div class="new-expense__control">
+                <div className="new-expense__control">
                     <label for="amount">Amount</label>
                     <input type="number" min="0.001" step="0.01" onChange={amountChangeHandler} />
                 </div>
 
 
 
-                <div class="new-expense__control">
+                <div className="new-expense__control">
                     <label for="Date">Date</label>
                     <input type="date" min="2019-01-01" max="2023-01-01" onChange={dateChangeHandler} />
                 </div>
             </div>
 
-            <div class="new-expense__actions">
+            <div className="new-expense__actions">
                 <button type="submit">Add submit</button>
             </div>
         </form>
